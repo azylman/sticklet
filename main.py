99c184235f9341b997865d0e30bc6b3bb7c84fb0
Alex Zylman <alex.zylman@gmail.com>
2011-08-08 21:29:03 -0600

    add static css and fix tabs

--- main.py
+++ main.py
@@ -10,62 +10,62 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class StickyNote(db.Model):
-  """Models an individual Guestbook entry with an author, content, and date."""
-  author = db.UserProperty()
-  content = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
-  subject = db.StringProperty
+	"""Models an individual Guestbook entry with an author, content, and date."""
+	author = db.UserProperty()
+	content = db.StringProperty(multiline=True)
+	date = db.DateTimeProperty(auto_now_add=True)
+	subject = db.StringProperty
 
 def guestbook_key(guestbook_name=None):
-  """Constructs a datastore key for a Guestbook entity with guestbook_name."""
-  return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
+	"""Constructs a datastore key for a Guestbook entity with guestbook_name."""
+	return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
 
 class MainPage(webapp.RequestHandler):
-    def get(self):
-        guestbook_name=self.request.get('guestbook_name')
-        greetings_query = StickyNote.all().ancestor(
-            guestbook_key(guestbook_name)).order('-date')
-        greetings = greetings_query.fetch(10)
+		def get(self):
+				guestbook_name=self.request.get('guestbook_name')
+				greetings_query = StickyNote.all().ancestor(
+						guestbook_key(guestbook_name)).order('-date')
+				greetings = greetings_query.fetch(10)
 
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+				if users.get_current_user():
+						url = users.create_logout_url(self.request.uri)
+						url_linktext = 'Logout'
+				else:
+						url = users.create_login_url(self.request.uri)
+						url_linktext = 'Login'
 
-        template_values = {
-            'greetings': greetings,
-            'url': url,
-            'url_linktext': url_linktext,
-        }
+				template_values = {
+						'greetings': greetings,
+						'url': url,
+						'url_linktext': url_linktext,
+				}
 
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
+				path = os.path.join(os.path.dirname(__file__), 'index.html')
+				self.response.out.write(template.render(path, template_values))
 
 class Guestbook(webapp.RequestHandler):
-  def post(self):
-    # We set the same parent key on the 'StickyNote' to ensure each greeting is in
-    # the same entity group. Queries across the single entity group will be
-    # consistent. However, the write rate to a single entity group should
-    # be limited to ~1/second.
-    guestbook_name = self.request.get('guestbook_name')
-    greeting = StickyNote(parent=guestbook_key(guestbook_name))
+	def post(self):
+		# We set the same parent key on the 'StickyNote' to ensure each greeting is in
+		# the same entity group. Queries across the single entity group will be
+		# consistent. However, the write rate to a single entity group should
+		# be limited to ~1/second.
+		guestbook_name = self.request.get('guestbook_name')
+		greeting = StickyNote(parent=guestbook_key(guestbook_name))
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
+		if users.get_current_user():
+			greeting.author = users.get_current_user()
 
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
+		greeting.content = self.request.get('content')
+		greeting.put()
+		self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
 
 application = webapp.WSGIApplication([
-  ('/', MainPage),
-  ('/sign', Guestbook)
+	('/', MainPage),
+	('/sign', Guestbook)
 ], debug=True)
 
 def main():
-    run_wsgi_app(application)
+		run_wsgi_app(application)
 
 if __name__ == "__main__":
-    main()
+		main()
