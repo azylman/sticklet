4f7284287b69dd8c802ed839ed8a51df40bd391b
Alex Zylman <alex.zylman@gmail.com>
2011-08-09 01:02:46 -0600

    added customizability to the way the dictionary is created

--- notes.py
+++ notes.py
@@ -1,3 +1,8 @@
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 import cgi
 import wsgiref.handlers
 
--- stickynote.py
+++ stickynote.py
@@ -1,10 +1,6 @@
 from google.appengine.ext import db
 
 class StickyNoteModel(db.Model):
-	def to_dict(self):
-		return dict([(p, unicode(getattr(self, p))) for p in self.properties()])
-
-class snModel(StickyNoteModel):
 	"""Models an individual Guestbook entry with an author, content, and date."""
 	author = db.UserProperty()
 	content = db.StringProperty(multiline=True)
@@ -13,6 +9,19 @@ class snModel(StickyNoteModel):
 	x = db.FloatProperty()
 	y = db.FloatProperty()
 	z = db.IntegerProperty()
+
+class snModel(StickyNoteModel):
+	def to_dict(self):
+		#return dict([(p, unicode(getattr(self, p))) for p in self.properties()])
+		return dict({
+			"id": unicode(self.key),
+			"author" : unicode(self.author),
+			"content" : self.content,
+			"date" : unicode(self.date),
+			"subject" : self.subject,
+			"x" : self.x,
+			"y" : self.y,
+			"z" : self.z})
 
 def key(email):
 	"""Constructs a datastore key for a Guestbook entity with guestbook_name."""
