c7c35fa7a1c63446c7b97796c484f0db9f232f21
vincekd <vincekd@gmail.com>
2011-08-09 15:55:47 -0500

    z-index ordering

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 var dragged = {};
+var z = 0;
 function startDrag ( e ) {
+
+    event.stopPropagation();
 
     var el = e.target;
     var edd = e.currentTarget;
 
-//    while ( el != null && el.nodeName != "BODY" && ! /^note$/.test ( el.className ) )
-//	el = el.parentNode;
-
-//    if ( el.nodeName == "BODY" || el.nodeName == null || el.nodeName == undefined) return;
     dragged.el = edd;
+
+    edd.style.zIndex = ++z;
 
     dragged.x = e.clientX + window.scrollX;
     dragged.y = e.clientY + window.scrollY;
@@ -17,11 +18,11 @@ function startDrag ( e ) {
 
     document.addEventListener( "mousemove", dragging, true );
     document.addEventListener( "mouseup", stopDrag, true );
-
-    event.stopPropagation();
 }
 
 function dragging ( e ) {
+
+    e.stopPropagation();
 
     var x = e.clientX + window.scrollX;
     var y = e.clientY + window.scrollY;
@@ -29,16 +30,13 @@ function dragging ( e ) {
     dragged.el.style.left = (dragged.sx + x - dragged.x) + "px";
     dragged.el.style.top = (dragged.sy + y - dragged.y) + "px";
 
-    e.stopPropagation();
-
 }
 
 function stopDrag ( e ) {
-
-    //save position with ajax call
-    //var submit = JSON.stringify ( dragged.el );
-    //console.log ( dragged );
     
+    e.stopPropagation();
+    //save position with ajax call
+
     document.removeEventListener( "mousemove", dragging, true );
     document.removeEventListener( "mouseup", stopDrag, true );
     dragged = {};
