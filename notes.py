a8b8d9986e899e74dc5da2b5a235f1352b889f20
vincekd <vincekd@gmail.com>
2011-08-19 12:17:28 -0500

    Check to see it's been saved before deleting it

--- notes.py
+++ notes.py
@@ -118,7 +118,8 @@ class Trash(webapp.RequestHandler):
 			dict =  json.loads ( self.request.body )
 			for note in dict:
 				db_n = stickynote.db.get( note['id'] )
-				db_n.delete()
+				if db_n.is_saved():
+					db_n.delete()
 		else:
 			self.error(401)
 			self.response.out.write("Not logged in.")
